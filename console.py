fcb58ea04c2d4431ff84a5b06fd41953cb5c9514
SandileG <sandilegraham@gmail.com>
2024-02-16 23:24:26 -0800

    ?

---
+++ console.py
@@ -0,0 +1,217 @@
+#!/usr/bin/python3
+"""Module for the entry point of the command interpreter."""
+
+import cmd
+from models.base_model import BaseModel
+from models import storage
+import re
+import json
+
+
+class CommandInterpreter(cmd.Cmd):
+
+    """Class for the command interpreter."""
+
+    prompt = "(hbnb) "
+
+    def default_command(self, line):
+        """Catch commands if nothing else matches then."""
+        self._pre_command(line)
+
+    def _pre_command(self, line):
+        """Intercepts commands to test for class.syntax()"""
+        match = re.search(r"^(\w*)\.(\w+)(?:\(([^)]*)\))$", line)
+        if not match:
+            return line
+        class_name = match.group(1)
+        method_name = match.group(2)
+        arguments = match.group(3)
+        match_uid_and_args = re.search('^"([^"]*)"(?:, (.*))?$', arguments)
+        if match_uid_and_args:
+            uid = match_uid_and_args.group(1)
+            attr_or_dict = match_uid_and_args.group(2)
+        else:
+            uid = arguments
+            attr_or_dict = False
+
+        attribute_and_value = ""
+        if method_name == "update" and attr_or_dict:
+            match_dict = re.search('^({.*})$', attr_or_dict)
+            if match_dict:
+                self.update_dict(class_name, uid, match_dict.group(1))
+                return ""
+            match_attr_and_value = re.search(
+                '^(?:"([^"]*)")?(?:, (.*))?$', attr_or_dict)
+            if match_attr_and_value:
+                attribute_and_value = (match_attr_and_value.group(
+                    1) or "") + " " + (match_attr_and_value.group(2) or "")
+        command = f"{method_name} {class_name} {uid} {attribute_and_value}"
+        self.onecmd(command)
+        return command
+
+    def update_dict(self, class_name, uid, s_dict):
+        """Helper method for update() with a dictionary."""
+        s = s_dict.replace("'", '"')
+        d = json.loads(s)
+        if not class_name:
+            print("** class name missing **")
+        elif class_name not in storage.classes():
+            print("** class doesn't exist **")
+        elif uid is None:
+            print("** instance id missing **")
+        else:
+            key = "{}.{}".format(class_name, uid)
+            if key not in storage.all():
+                print("** no instance found **")
+            else:
+                attributes = storage.attributes()[class_name]
+                for attribute, value in d.items():
+                    if attribute in attributes:
+                        value = attributes[attribute](value)
+                    setattr(storage.all()[key], attribute, value)
+                storage.all()[key].save()
+
+    def do_EOF(self, line):
+        """Handles End Of File character.
+        """
+        print()
+        return True
+
+    def do_quit(self, line):
+        """Exits the program.
+        """
+        return True
+
+    def empty_line(self):
+        """Doesn't do anything on ENTER.
+        """
+        pass
+
+    def do_create(self, line):
+        """Creates an instance.
+        """
+        if line == "" or line is None:
+            print("** class name missing **")
+        elif line not in storage.classes():
+            print("** class doesn't exist **")
+        else:
+            obj = storage.classes()[line]()
+            obj.save()
+            print(obj.id)
+
+    def do_show(self, line):
+        """Prints the string representation of an instance.
+        """
+        if line == "" or line is None:
+            print("** class name missing **")
+        else:
+            words = line.split(' ')
+            if words[0] not in storage.classes():
+                print("** class doesn't exist **")
+            elif len(words) < 2:
+                print("** instance id missing **")
+            else:
+                key = "{}.{}".format(words[0], words[1])
+                if key not in storage.all():
+                    print("** no instance found **")
+                else:
+                    print(storage.all()[key])
+
+    def do_destroy(self, line):
+        """Deletes an instance based on the class name and id.
+        """
+        if line == "" or line is None:
+            print("** class name missing **")
+        else:
+            words = line.split(' ')
+            if words[0] not in storage.classes():
+                print("** class doesn't exist **")
+            elif len(words) < 2:
+                print("** instance id missing **")
+            else:
+                key = "{}.{}".format(words[0], words[1])
+                if key not in storage.all():
+                    print("** no instance found **")
+                else:
+                    del storage.all()[key]
+                    storage.save()
+
+    def do_all_instances(self, line):
+        """Prints all string representation of all instances.
+        """
+        if line != "":
+            words = line.split(' ')
+            if words[0] not in storage.classes():
+                print("** class doesn't exist **")
+            else:
+                nl = [str(obj) for key, obj in storage.all().items()
+                      if type(obj).__name__ == words[0]]
+                print(nl)
+        else:
+            new_list = [str(obj) for key, obj in storage.all().items()]
+            print(new_list)
+
+    def do_count_instances(self, line):
+        """Counts the instances of a class.
+        """
+        words = line.split(' ')
+        if not words[0]:
+            print("** class name missing **")
+        elif words[0] not in storage.classes():
+            print("** class doesn't exist **")
+        else:
+            matches = [
+                k for k in storage.all() if k.startswith(
+                    words[0] + '.')]
+            print(len(matches))
+
+    def do_update_instance(self, line):
+        """Updates an instance by adding or updating attribute.
+        """
+        if line == "" or line is None:
+            print("** class name missing **")
+            return
+
+        rex = r'^(\S+)(?:\s(\S+)(?:\s(\S+)(?:\s((?:"[^"]*")|(?:(\S)+)))?)?)?'
+        match = re.search(rex, line)
+        class_name = match.group(1)
+        uid = match.group(2)
+        attribute = match.group(3)
+        value = match.group(4)
+        if not match:
+            print("** class name missing **")
+        elif class_name not in storage.classes():
+            print("** class doesn't exist **")
+        elif uid is None:
+            print("** instance id missing **")
+        else:
+            key = "{}.{}".format(class_name, uid)
+            if key not in storage.all():
+                print("** no instance found **")
+            elif not attribute:
+                print("** attribute name missing **")
+            elif not value:
+                print("** value missing **")
+            else:
+                cast = None
+                if not re.search('^".*"$', value):
+                    if '.' in value:
+                        cast = float
+                    else:
+                        cast = int
+                else:
+                    value = value.replace('"', '')
+                attributes = storage.attributes()[class_name]
+                if attribute in attributes:
+                    value = attributes[attribute](value)
+                elif cast:
+                    try:
+                        value = cast(value)
+                    except ValueError:
+                        pass  # fine, stay a string then
+                setattr(storage.all()[key], attribute, value)
+                storage.all()[key].save()
+
+
+if __name__ == '__main__':
+    CommandInterpreter().cmdloop()
---
+++ one/README.md
@@ -0,0 +1,41 @@
+# Airbnb Clone Project
+
+This project aims to develop an Airbnb clone, focusing on building a robust backend system with Python. The project will cover various aspects of web development, including creating a command-line interface, implementing storage mechanisms, building a web server, and exposing data via a RESTful API.
+
+## Project Overview
+
+The project will be developed step by step, gradually adding features and functionalities. Each step will introduce new concepts and build upon the previous ones. The primary components and concepts covered in this project include:
+
+### Components:
+
+1. **Console:** A command-line interface (CLI) for managing objects within the application, including creating, updating, and deleting objects.
+
+2. **Web Static:** Development of static HTML templates for the application's frontend, including learning HTML/CSS and creating template files for each object.
+
+3. **MySQL Storage:** Integration of MySQL database storage, mapping Python models to database tables using an ORM (Object-Relational Mapper).
+
+4. **Web Framework - Templating:** Creation of a web server in Python and making static HTML files dynamic by integrating them with the database.
+
+5. **RESTful API:** Exposing objects stored in the database via a JSON-based web interface and enabling manipulation of objects through a RESTful API.
+
+6. **Web Dynamic:** Learning JQuery and loading objects dynamically from the client-side using the RESTful API.
+
+### Concepts Covered:
+
+- **Creating a Python Package:** Organizing code into modules and submodules to create a Python package, providing modularity and code organization benefits.
+
+- **Creating a Command Interpreter in Python:** Utilizing the `cmd` module to build a command-line interpreter for interacting with the application.
+
+- **Unit Testing:** Implementing unit tests to ensure code correctness and robustness, particularly in a large project, using the Python `unittest` module.
+
+- **Serialization and Deserialization:** Converting Python objects into a serializable format (serialization) and reconstructing them back into Python objects (deserialization), often using JSON format for data interchange.
+
+- **Reading and Writing JSON Files:** Handling JSON files for storing and retrieving data, providing a human-readable and standardized format for data storage.
+
+- **Managing Datetime:** Manipulating date and time data in Python using the `datetime` module, including formatting dates and times, performing arithmetic operations, and handling timezone information.
+
+- **UUID (Universally Unique Identifier):** Utilizing UUIDs for generating unique identifiers for objects, ensuring uniqueness across distributed systems.
+
+- **`*args` and `**kwargs`:** Understanding and utilizing variable-length argument lists in Python functions using `*args` and `**kwargs` syntax, enabling flexibility in function definitions.
+
+- **Named Arguments in Functions:** Handling named arguments in function definitions and function calls, allowing for clearer and more expressive code.
